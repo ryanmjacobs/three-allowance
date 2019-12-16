ec923295b995d4270b7896f5aacdd5f00daf9c9b
Ryan Jacobs <ryan@rmj.us>
2019-12-16 13:44:06 -0500

    actually execute + more logs

--- app.js
+++ app.js
@@ -5,30 +5,35 @@ const {Client} = require("pg");
 const puppeteer = require("puppeteer");
 
 async function scrape() {
+  console.log("Launching browser...");
   const browser = await puppeteer.launch();
+
+  console.log("Opening page...");
   const page = await browser.newPage()
   await page.setViewport({ width: 1280, height: 1800 })
   await page.goto("https://www.three.co.uk/My3Account2018/My3Login");
 
-  // login
+  console.log("Signing in...");
   await page.click("#my3_login_form > label > span > input");
   await page.keyboard.type(process.env.THREE_ID);
   await page.click("#my3_login_form > div.magicpassword > label:nth-child(1) > input");
   await page.keyboard.type(process.env.THREE_PASSWORD);
   await page.click("#my3-login-submit");
 
-  // navigate to balance page
+  console.log("Navigating to balance page...");
   await page.waitForNavigation();
   await page.goto("https://www.three.co.uk/New_My3/Data_allowance?id=My3_DataAllowanceHeading");
 
-  // grab balance
+  console.log("Scraping balance...");
   const remaining_data_mb = await page.evaluate(
     sel => parseInt(document.querySelector(sel).innerHTML),
     "#pl-top > div.threePortlet.P30_id.P30_checkMyBalance_w2 > table > tbody:nth-child(3) > tr > td.alignRight");
 
+  console.log("Inserting into Postgres...");
   console.log({remaining_data_mb});
   await insert_pg(remaining_data_mb);
 
+  console.log("Closing browser...");
   await browser.close()
 }
 
@@ -38,9 +43,11 @@ async function insert_pg(remaining_data_mb) {
 
   const host = `${os.userInfo().username}@${os.hostname()}`;
   const res = await client.query(
-    "INSERT INTO acpi_log (host, cmd, content) VALUES ($1, '$2', '$3')",
+    "INSERT INTO acpi_log (host, cmd, content) VALUES ($1, $2, $3)",
     [host, "three.co.uk remaining data balance", {remaining_data_mb}]
   );
 
   await client.end()
 }
+
+scrape().then(process.exit);
