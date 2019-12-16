ded358c1cabc2dd2bbf9762fcd207b0ee2f8429d
Ryan Jacobs <ryan@rmj.us>
2019-12-16 13:16:39 -0500

    screenshot

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 #!/usr/bin/env node
+
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.setViewport({ width: 1280, height: 1800 })
+
+  await page.goto("https://https://www.three.co.uk/My3Account2018/My3Login");
+  await page.screenshot({ path: 'screenshot.png' });
+
+  await browser.close()
+})();
